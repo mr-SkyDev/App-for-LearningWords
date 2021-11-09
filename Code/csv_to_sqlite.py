6d54a75dd8832b3d4a292238ceca627740748315
SkyGuy <nurmagomedoffzaur@yandex.ru>
2021-11-09 19:00:46 +0300

    finish englishSlangCourse for db

---
+++ Code/csv_to_sqlite.py
@@ -0,0 +1,27 @@
+import csv
+import sqlite3
+
+
+def main():
+    filename = "englishSlangCourse"
+    with open('WordsDB/' + filename + '.csv', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(
+            csvfile, fieldnames=["word", "value"], quotechar='"', delimiter=";"
+        )
+        reader = list(reader)
+
+        con = sqlite3.connect('WordsDB/words.db')
+        for line in reader:
+            cur = con.cursor()
+            word, value = line['word'], line['value']
+
+            query = f"""
+                INSERT INTO {filename} (word, value, is_using)
+                VALUES ("{word}", "{value}", 1)
+            """
+            cur.execute(query)
+            con.commit()
+
+
+if __name__ == "__main__":
+    main()
